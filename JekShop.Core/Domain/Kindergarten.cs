7e676d6aae9faf0a3114514af5d3cc4916ed2f4d
«JekGuz» <jekaterina.guzek@gmail.com>
2025-09-16 18:47:37 +0300

    domain -> class

--- JekShop.Core/Domain/Kindergarten.cs
+++ JekShop.Core/Domain/Kindergarten.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JekShop.Core.Domain
+﻿namespace JekShop.Core.Domain
 {
-    public interface Kindergarten
+    public class Kindergarten
     {
         public Guid Id { get; set; }
         public string GroupName { get; set; }
