5ac24361b1c92eeda5e6c7ed92625a05ea8a44f9
«JekGuz» <jekaterina.guzek@gmail.com>
2025-09-16 18:28:33 +0300

    IKindergartenServices

---
+++ JekShop.Core/ServiceInterface/IKindergartenServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JekShop.Core.Domain;
+using JekShop.Core.Dto;
+
+namespace JekShop.Core.ServiceInterface
+{
+    internal class IKindergartenServices
+    {
+        Task<Kindergarten> Create(KindergartenDto dto);
+        Task<Kindergarten> DetailAsync(Guid id);
+        Task<Kindergarten> Delete(Guid id);
+        Task<Kindergarten> Update(KindergartenDto dto);
+    }
+}
