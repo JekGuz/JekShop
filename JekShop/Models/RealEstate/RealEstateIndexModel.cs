3e79dae516befb0db18a849e3285dc70de0d3fd1
«JekGuz» <jekaterina.guzek@gmail.com>
2025-09-30 17:10:23 +0300

    RealEstateIndexModel

---
+++ JekShop/Models/RealEstate/RealEstateIndexModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace JekShop.Models.RealEstate
+{
+    public class RealEstateIndexModel
+    {
+        public Guid? Id { get; set; }
+        public double? Area { get; set; }
+        public string? Location { get; set; }
+        public int? RoomNumber { get; set; }
+        public string? BuildingType { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
