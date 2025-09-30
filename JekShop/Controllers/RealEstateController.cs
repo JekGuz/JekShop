af79f5b7394d21775b43d8873f970dc832481bd4
«JekGuz» <jekaterina.guzek@gmail.com>
2025-09-30 14:31:13 +0300

    Controller --- Veiw kaust

---
+++ JekShop/Controllers/RealEstateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JekShop.Controllers
+{
+    public class RealEstateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
