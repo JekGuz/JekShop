21a345d4b80835d9a20d0491ddc32238ef8da255
«JekGuz» <jekaterina.guzek@gmail.com>
2025-10-12 21:06:40 +0300

    DTO and Domain

--- JekShop.Core/Domain/FileToDatabase.cs
+++ JekShop.Core/Domain/FileToDatabase.cs
@@ -1,10 +1,10 @@
-﻿namespace JekShop.Core.Dto
+﻿namespace JekShop.Core.Domain
 {
-    public class FileToDatabaseDto
+    public class FileToDatabase
     {
         public Guid Id { get; set; }
         public string? ImageTitle { get; set; }
         public byte[]? ImageData { get; set; }
-        public Guid? RealEstateId { get; set; }
+        public Guid? KindergartenId { get; set; }
     }
 }
