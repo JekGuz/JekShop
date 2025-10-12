43abf76cafb37c75c3d620cdb38710d338b6a447
«JekGuz» <jekaterina.guzek@gmail.com>
2025-10-12 21:22:21 +0300

    View - Delete

--- JekShop/Models/Kindergartens/KindergartenDeleteViewModel.cs
+++ JekShop/Models/Kindergartens/KindergartenDeleteViewModel.cs
@@ -7,8 +7,8 @@
         public int? ChildrenCount { get; set; }
         public string? KindergartenName { get; set; }
         public string? TeacherName { get; set; }
-
-
+        public List<KindergartenImageViewModel> Images { get; set; }
+= new List<KindergartenImageViewModel>();
         public DateTime? CreateAt { get; set; }
         public DateTime? UpdateAt { get; set; }
     }
