38a751ee40898d01eb1e67ada387747496983d73
«JekGuz» <jekaterina.guzek@gmail.com>
2025-10-13 17:05:05 +0300

    AccuLocationRootDto - 1 classis

--- JekShop.Core/Dto/AccuLocationRootDto.cs
+++ JekShop.Core/Dto/AccuLocationRootDto.cs
@@ -4,18 +4,38 @@ namespace JekShop.Core.Dto
 {
     public class AccuLocationRootDto
     {
-        public DataSetDateTime? LocalObservatioDateTime { get; set; }
+        public string? LocalObservatioDateTime { get; set; }
         public int? EpochTime { get; set; }
         public string? WeatherText { get; set; }
         public int? WeatherIcon { get; set; }
         public bool? HasPrecipitation { get; set; }
         public string? PrecipitationType { get; set; }
         public bool? IsDayTime { get; set; }
-        public AccuLocationTemperatureDto? Temperature { get; set; }
-        public AccuLocationImperialDto? Imperial { get; set; }
+        public TemperatureDto? Temperature { get; set; }
+        public ImperialDto? Imperial { get; set; }
         public string? MobileLink { get; set; }
         public string? Link { get; set; }
+    }
 
+    public class TemperatureDto
+    {
+        public MetricDto? Metric { get; set; }
+    }
 
+    public class MetricDto
+    {
+        public double? Value { get; set; }
+        public string? Unit { get; set; }
+        public int? UnitType { get; set; }
+    }
+
+    public class ImperialDto
+    {
+        public double? Value { get; set; }
+        public string? Unit { get; set; }
+        public int? UnitType { get; set; }
     }
 }
+
+
+
