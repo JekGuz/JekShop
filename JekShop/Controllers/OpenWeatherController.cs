1712b9a597ad5bf2a88d4f5e5c7af548a6825a56
«JekGuz» <jekaterina.guzek@gmail.com>
2025-10-28 18:59:21 +0200

    OpenWeatherController

---
+++ JekShop/Controllers/OpenWeatherController.cs
@@ -0,0 +1,15 @@
+﻿using JekShop.Core.ServiceInterface;
+using Microsoft.AspNetCore.Mvc;
+
+public class OpenWeatherController : Controller
+{
+    private readonly IOpenWeatherService _svc;
+    public OpenWeatherController(IOpenWeatherService svc) => _svc = svc;
+
+    [HttpGet]
+    public async Task<IActionResult> Index(string city = "Tallinn", string units = "metric")
+    {
+        var dto = await _svc.GetCurrentByCityAsync(city, units);
+        return View(dto);
+    }
+}
