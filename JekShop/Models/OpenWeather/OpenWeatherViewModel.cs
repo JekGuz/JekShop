0f172ee1e9c15037b08743b03c59c859c4399804
«JekGuz» <jekaterina.guzek@gmail.com>
2025-10-28 19:01:30 +0200

    OpenWeatherViewModel

---
+++ JekShop/Models/OpenWeather/OpenWeatherViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace JekShop.Models.OpenWeather
+{
+    public class OpenWeatherViewModel
+    {
+        public string? City { get; set; }
+        public string? Country { get; set; }
+
+        public double? Temp { get; set; }
+        public double? FeelsLike { get; set; }
+        public int? Humidity { get; set; }
+        public double? WindSpeed { get; set; }
+
+        public string? Description { get; set; }
+        public string? IconUrl { get; set; } // https://openweathermap.org/img/wn/{icon}@2x.png  очень мило будет :)
+        public string? Units { get; set; } // для подписи °C / °F
+    }
+}
