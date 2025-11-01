4e590d9eb02229a0ae1a0ffb01112f2deeb00e08
«JekGuz» <jekaterina.guzek@gmail.com>
2025-11-01 22:31:46 +0200

    OpenWeatherViewModel

--- JekShop/Models/OpenWeather/OpenWeatherViewModel.cs
+++ JekShop/Models/OpenWeather/OpenWeatherViewModel.cs
@@ -4,14 +4,25 @@
     {
         public string? City { get; set; }
         public string? Country { get; set; }
+        public string? Units { get; set; }
 
         public double? Temp { get; set; }
         public double? FeelsLike { get; set; }
         public int? Humidity { get; set; }
         public double? WindSpeed { get; set; }
-
         public string? Description { get; set; }
-        public string? IconUrl { get; set; } // https://openweathermap.org/img/wn/{icon}@2x.png  очень мило будет :)
-        public string? Units { get; set; } // для подписи °C / °F
+        public string? IconUrl { get; set; }
+
+        // добавим список на 1 или 3 дня
+        public List<DailyForecastVm> Days { get; set; } = new();
+    }
+
+    public class DailyForecastVm
+    {
+        public DateOnly Date { get; set; }
+        public double? Min { get; set; }
+        public double? Max { get; set; }
+        public string? Description { get; set; }
+        public string? IconUrl { get; set; }
     }
 }
