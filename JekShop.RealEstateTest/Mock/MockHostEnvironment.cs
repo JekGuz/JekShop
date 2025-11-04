887cdac9fcd64984289fb8256c8f1efcf8f5d5e6
«JekGuz» <jekaterina.guzek@gmail.com>
2025-11-04 17:24:45 +0200

    MockHostEnvironment

--- JekShop.RealEstateTest/Mock/MockHostEnvironment.cs
+++ JekShop.RealEstateTest/Mock/MockHostEnvironment.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 
 namespace JekShop.RealEstateTest.Mock
 {
-    public class MockHostEnvironment
+    public class MockHostEnvironment : IHostEnvironment
     {
+
+        public string EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        ////В каком окружении «работает» приложение
+        ////public string EnvironmentName { get; set; }
+
+        ////Имя приложения(можно любое)
+        ////public string ApplicationName { get; set; }
+
+        ////Корневая папка приложения
+        ////public string ContentRootPath { get; set; }
+
+        ////Провайдер файлов для этой папки
+        ////public IFileProvider ContentRootFileProvider { get; set; }
+
+        ////public MockHostEnvironment()
+        ////{
+        ////    Берём текущую папку тестового проекта
+        ////   ContentRootPath = Directory.GetCurrentDirectory();
+        ////    ContentRootFileProvider = new PhysicalFileProvider(ContentRootPath);
+        ////}
     }
 }
